77b470164ed46891b5afe16a93af0272a04b851c
xcd <111914552+thedotun@users.noreply.github.com>
2026-06-16 17:30:00 +0100

    fix for the domain

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       ),
     },
   },
-  base: process.env.NODE_ENV === "production" ? "/editor/" : "/",
+  base: "/",
   build: {
     rollupOptions: {
       output: {
